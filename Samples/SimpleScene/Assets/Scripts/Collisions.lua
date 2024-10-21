220873e72bf5e7ac87ad9785212f0bd5ff0b8283
MindamSer <androsovy.mail.ru@gmail.com>
2024-10-21 23:37:00 +0300

    working collisions!

--- Samples/SimpleScene/Assets/Scripts/Collisions.lua
+++ Samples/SimpleScene/Assets/Scripts/Collisions.lua
@@ -28,28 +28,26 @@ end
 
 
 
+local function oneCollision(it)
+	x, y, z, ent = it.param[1], it.param[2], it.param[3], it.param[4]
+	
+	for other_pos, other_coll, other_ent in ecs.each(it) do
+		if ent ~= other_ent then
+			local dist2 =	
+				(x - other_pos.x) * (x - other_pos.x) +
+				(y - other_pos.y) * (y - other_pos.y) +
+				(z - other_pos.z) * (z - other_pos.z)
+			if dist2 < 1.0 then
+				it.interrupted_by = 1
+			end
+		end
+    end
+end
+oneCollisionId = ecs.system(oneCollision, "OneCollisionSystem", ecs.Empty, "Position, Collider")
+
 local function collisions(it)
-	local dist2 = 0
-    for pos, coll, ent in ecs.each(it) do
-		coll.value = true
-		
-		--local q = ecs.query("Position, Collider, AppContext($)")
-        --local other_it = ecs.query_iter(q)
-		--while ecs.query_next(other_it) do
-		--	for other_pos, other_coll, other_ent in ecs.each(other_it) do
-		--		if not coll.value and ent.name ~= other_ent.name then
-		--			dist2 =	
-		--				(pos.x - other_pos.x) * (pos.x - other_pos.x) +
-		--				(pos.y - other_pos.y) * (pos.y - other_pos.y) +
-		--				(pos.z - other_pos.z) * (pos.z - other_pos.z)
-		--			if dist2 < 1.0 then
-		--				coll.value = true
-		--				
-		--				pos.y = 15.
-		--			end
-		--		end
-		--	end
-		--end
+	for pos, coll, ent in ecs.each(it) do
+		coll.value = ecs.run(oneCollisionId, 0.0, {pos.x, pos.y, pos.z, ent})
     end
 end
 
@@ -75,6 +73,6 @@ ecs.system(timeToLive, "TimeToLiveSystem", ecs.OnUpdate, "TimeToLive")
 ecs.system(deleteOnDeath, "DeletingSystem", ecs.OnUpdate, "Position, TimeToLive")
 ecs.system(dieOnGround, "DieOnGroundSystem", ecs.OnUpdate, "Position, BouncePlane, TimeToLive, DieOnGround")
 
-ecs.system(collisions, "CollisionsSystem", ecs.OnUpdate, "Position, Collider")
+ecs.system(collisions, "CollisionSystem", ecs.OnUpdate, "Position, Collider")
 ecs.system(jumpOnCollision, "JumpOnCollisionSystem", ecs.OnUpdate, "Velocity, Collider, JumpOnCollision")
 ecs.system(wooshOnCollision, "WooshOnCollisionSystem", ecs.OnUpdate, "Velocity, Collider, WooshOnCollision")
